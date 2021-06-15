d46656cf18e4410dee788663f1504ac4fd9e4bca
captainAldi <renaldiyulvianda@yahoo.com>
2021-06-15 23:47:31 +0700

    add artisan command and task scheduler for notif

---
+++ app/Console/Commands/SendNotifCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Jobs\SendChatJob;
+use Ramsey\Uuid\Uuid;
+
+use DB;
+use App\Models\BillApprover;
+use App\Models\Bill;
+use App\Models\User;
+
+class SendNotifCommand extends Command
+{
+    
+    protected $signature = 'send:notif';
+
+    
+    protected $description = 'Send Notif to All Approver that not yet Approve Bills';
+
+    
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    public function handle()
+    {
+        
+        // Get Waiting Approver
+        $dataWaitingApprover = BillApprover::where('status', 'Waiting')
+                                ->get();
+        
+        // Send Notifikasi ke Telegram
+         foreach ($dataWaitingApprover as $key) {
+            
+            $dataWaitingBill = Bill::where('id', $key->bill_id)->first();
+            $dataUser        = User::where('email', $key->email)->first();
+
+            $chatID         = $dataUser->chat_id_telegram;
+            $judul          = $dataWaitingBill->judul;
+            $pembuka        = $dataUser->name;
+            $deskripsi      = $dataWaitingBill->deskripsi;
+            $fileInv        = $dataWaitingBill->file_inv;
+            $approveLink    = env('VUE_APP_URL').'/otr/bill/approve/'.$key->remember_token;
+
+            dispatch(new SendChatJob(
+                $chatID, 
+                $judul, 
+                $pembuka, 
+                $deskripsi, 
+                $fileInv, 
+                $approveLink)
+            );
+
+        }
+
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
+use App\Console\Commands\SendNotif;
+
 class Kernel extends ConsoleKernel
 {
     /**
@@ -13,7 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\SendNotifCommand::class,
     ];
 
     /**
@@ -24,6 +26,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+        
+        // $schedule->command('send:notif')->everyMinute();
+        $schedule->command('send:notif')->dailyAt('13:00');
+
     }
 }
